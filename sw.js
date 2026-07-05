1ed5e2ce362ab07717b2c1ee090021235057fdf6
satoshi335-spec <satoshi335@gmail.com>
2026-07-05 18:37:05 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,39 @@
+const VERSION = "gj-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "./icons/apple-touch-icon.png"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(VERSION).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== VERSION).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    caches.match(e.request, { ignoreSearch: true }).then(cached => {
+      const fetched = fetch(e.request)
+        .then(res => {
+          if (res.ok && new URL(e.request.url).origin === location.origin) {
+            const clone = res.clone();
+            caches.open(VERSION).then(c => c.put(e.request, clone));
+          }
+          return res;
+        })
+        .catch(() => cached);
+      return cached || fetched;
+    })
+  );
+});
